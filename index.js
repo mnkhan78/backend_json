594dbc91c857d755865f8f52b994d4fbab5630dc
mnkhan78 <naseem.khan9500078@gmail.com>
2025-12-23 20:21:54 +0530

    connected to frontend
    communicating with frontend successfully using CORS

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const userRoutes = require("./routes/user.route");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors")
+
+
 
 const app = express();
+
+app.use(cors());
+
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
